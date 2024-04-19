293d75476c0557f6da126c55651ad1a1876efea0
Евгений Шержуков <jek.s0000@yandex.ru>
2024-04-19 15:24:57 +0300

    First version

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -2,7 +2,6 @@ from rest_framework import permissions
 from rest_framework.permissions import SAFE_METHODS
 
 
-
 class IsAuthorModerAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
@@ -20,15 +19,15 @@ class IsModeratorOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and
-                request.user.role in ['moderator', 'admin'])
+            or (request.user.is_authenticated
+                and request.user.role in ['moderator', 'admin'])
         )
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and
-                request.user.role in ['moderator', 'admin'])
+            or (request.user.is_authenticated
+                and request.user.role in ['moderator', 'admin'])
         )
 
 
@@ -36,15 +35,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and
-                request.user.role == 'admin')
+            or (request.user.is_authenticated
+                and request.user.role == 'admin')
         )
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and
-                request.user.role == 'admin')
+            or (request.user.is_authenticated
+                and request.user.role == 'admin')
         )
 
 
@@ -62,6 +61,7 @@ class IsAuthorModAdminOrReadOnlyPermission(permissions.BasePermission):
             or obj.author == request.user
             or request.user.role in ['moderator', 'admin']
         )
+
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
